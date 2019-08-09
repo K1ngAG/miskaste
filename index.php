3dc676e7afed6924ef9a3c535e159823acb6df4d
Aivis Gailis <gailisaivis12@inbox.lv>
2019-08-09 04:13:20 +0300

    dompdf neiet

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+
+//readfile('index.html');
 
 
-readfile('index.html');
+// Composer's auto-loading functionality
+require "vendor/autoload.php";
+
+use Dompdf\Dompdf;
+
+//generate some PDFs!
+$dompdf = new DOMPDF();  //if you use namespaces you may use new \DOMPDF()
+$html = file_get_contents("index.html");
+$dompdf->loadHtml($html);
+$dompdf->render();
+$dompdf->stream("sample.pdf", array("Attachment"=>0));
